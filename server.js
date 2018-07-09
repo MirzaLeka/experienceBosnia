e1fd9ccd29c7b65f5d670909e01a4ce2b64ad65b
Mirza Leka <mirza_376@hotmail.com>
2018-07-09 21:09:23 +0200

    Minor updates to server and package files

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/web-info', {
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/Web-Info', {
 extensions: ['html', 'htm']
 }));
 
 app.get("/", (req, res) => {
-res.sendFile("index.html", {"root": __dirname + '/web-info'});
+res.sendFile("index.html", {"root": __dirname + '/Web-Info'});
 });
 
 
-app.use('/resources', express.static(__dirname + "/resources"));
-
-const port = process.env.PORT || 3000;
+app.use('/Resources', express.static(__dirname + "/Resources"));
 
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
