e028f2b161b66f3c2db518a669b38a35197ef2e8
Mirza Leka <mirza_376@hotmail.com>
2018-07-25 15:41:34 +0200

    Modified Server file. Added Error Page

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(require('body-parser').urlencoded({extended: false}));
 
 const port = process.env.PORT || 3000;
 
@@ -11,8 +13,28 @@ app.get("/", (req, res) => {
 res.sendFile("index.html", {"root": __dirname + '/Web-Info'});
 });
 
-
 app.use('/Resources', express.static(__dirname + "/Resources"));
+
+/* Render Error Page */
+
+const router = express.Router();
+
+router.use(function(req, res, next) {
+    if (!req.route)
+        return next (new Error('404'));  
+    next();
+});
+
+router.use(function(err, req, res, next){
+   // res.send(err.message);
+    res.sendFile("errorPage.html", { root: path.join(__dirname, '/Web-Info') });
+})
+
+router.use(function(req, res){
+    res.send(app.locals.test + '');
+});
+
+ app.use(router);
 
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
